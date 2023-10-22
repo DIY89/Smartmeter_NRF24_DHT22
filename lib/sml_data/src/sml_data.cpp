a793e410fead464da3964893750f881bf8b54dda
Steffen Thun <Steffen.Thun@ssi-schaefer.com>
2023-10-22 12:24:33 +0200

    implement sml_data routines

--- lib/sml_data/src/sml_data.cpp
+++ lib/sml_data/src/sml_data.cpp
@@ -1,11 +1,108 @@
 #include <sml_data.h>
 
-float current_pow = 0;
+double T1Wh = -2;
+double SumWh = -2; 
+double T1cur = -2;
+
+char sml_data[MAX_BUF_SIZE];
+unsigned char manuf[MAX_STR_MANUF];
+
+void PowerT1(){ 
+  smlOBISWh(T1Wh); 
+}
+
+void PowerSum(){ 
+  smlOBISWh(SumWh); 
+}
+
+void PowerCur(){ 
+  smlOBISW(T1cur); 
+}
+
+void Manufacturer(){
+  smlOBISManufacturer(manuf, MAX_STR_MANUF); 
+}
+
+// clang-format off
+OBISHandler OBISHandlers[] = {
+  {{ 0x81, 0x81, 0xc7, 0x82, 0x03, 0xff }, &Manufacturer}, /* 129-129:199.130.3*255 */
+  {{ 0x01, 0x00, 0x01, 0x08, 0x01, 0xff }, &PowerT1},      /*   1-  0:  1.  8.1*255 (T1) */
+  {{ 0x01, 0x00, 0x01, 0x08, 0x00, 0xff }, &PowerSum},     /*   1-  0:  1.  8.0*255 (T1 + T2) */
+  {{ 0x01, 0x00, 0x10, 0x07, 0x00, 0xff }, &PowerCur},     /*   1-  0: 16.  7.0*255 (Power currenctly W) */
+  {{ 0, 0 }}
+};
 
 void init_sml_data(){
  //Implemenation
 }
 
 void read_sml_data(){
- //Implemenation
+  char floatBuffer[20];
+  unsigned int i = 0, iHandler = 0;
+  unsigned char c;
+  sml_states_t s;
+  bool new_data = false;
+
+  if(Serial.available() > 0){
+    Serial.readBytes(sml_data,sizeof(sml_data));
+
+    new_data = true;
+  }
+
+  if (new_data == true) {
+#if defined(DEBUG)
+    Serial.println("new data available");
+#endif
+    for (i = 0; i < sizeof(sml_data); ++i) {
+      c = sml_data[i];
+      s = smlState(c);
+      if (s == SML_START) {
+        //Serial.println("SML_START");
+        /* reset local vars */
+        T1Wh = -3;
+        SumWh = -3;
+      }
+      if (s == SML_LISTEND) {
+        //Serial.println("SML_LISTEND");
+        /* check handlers on last received list */
+        for (iHandler = 0; OBISHandlers[iHandler].Handler != 0 &&
+                           !(smlOBISCheck(OBISHandlers[iHandler].OBIS));
+             iHandler++)
+             ;
+        if (OBISHandlers[iHandler].Handler != 0) {
+          OBISHandlers[iHandler].Handler();
+          //Serial.println("Handler");
+        }
+      }
+#if defined(DEBUG)
+      if (s == SML_UNEXPECTED) {
+        //Serial.print(F(">>> Unexpected byte! <<<\n"));
+      }
+
+      if (s == SML_FINAL) {
+        Serial.println("SML_FINAL");
+
+        Serial.print(F(">>> Manufacturer :"));
+        Serial.print((char *)manuf);
+        Serial.print(F("\n")); 
+        
+        Serial.print(F("Power T1    (1-0:1.8.1)..: "));
+        dtostrf(T1Wh, 10, 3, floatBuffer);
+        Serial.print(floatBuffer);
+        Serial.print(F("\n"));
+
+        Serial.print(F("Power T1+T2 (1-0:1.8.0)..: "));
+        dtostrf(SumWh, 10, 3, floatBuffer);
+        Serial.print(floatBuffer);
+        Serial.print(F("\n"));
+
+        Serial.print(F("Power Currently    (1-0:16.7.0)..: "));
+        dtostrf(T1cur, 10, 3, floatBuffer);
+        Serial.print(floatBuffer);
+        Serial.print(F("\n\n\n\n"));
+      }
+#endif
+    }
+    new_data = false;
+  }
 }
--- lib/sml_data/src/sml_data.h
+++ lib/sml_data/src/sml_data.h
@@ -1,9 +1,27 @@
-
 #ifndef SML_DATA_H
 #define SML_DATA_H
 
-extern float current_pow;
+#include <sml.h>
+#include <Arduino.h>
 
+#define MAX_BUF_SIZE 400
+#define MAX_STR_MANUF 5
+// Continuously loops through a static message from RAM and outputs information
+// to serial
+
+extern double T1Wh;
+extern double SumWh; 
+extern double T1cur;
+
+typedef struct {
+  const unsigned char OBIS[6];
+  void (*Handler)();
+} OBISHandler;
+
+void PowerT1();
+void PowerSum();
+void PowerCur();
+void Manufacturer();
 void init_sml_data();
 void read_sml_data();
 
